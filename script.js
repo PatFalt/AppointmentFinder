7a3518836c4205bbb969b0bf2624cbb5301e29ff
PatFalt <81306186+PatFalt@users.noreply.github.com>
2022-04-12 00:26:22 +0200

    a

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 "use strict";
 console.log("works");
 alert("works");
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.querySelector("#alarm");
+    alert("Page loaded");
+    button === null || button === void 0 ? void 0 : button.addEventListener("click", function () {
+        alert("Test worked");
+    });
+});
--- script.ts
+++ script.ts
@@ -1,2 +1,10 @@
 console.log("works");
 alert("works");
+
+document.addEventListener("DOMContentLoaded", function() {
+    let button = document.querySelector("#alarm");
+    alert("Page loaded");
+    button?.addEventListener("click", function(){
+        alert("Test worked");
+    })
+});
