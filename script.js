ff04ef7149dd53af1fa7f226d32fdaea1d907680
PatFalt <81306186+PatFalt@users.noreply.github.com>
2022-04-12 22:44:37 +0200

    ERM

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 "use strict";
-console.log("works");
-alert("works");
 document.addEventListener("DOMContentLoaded", function () {
     let button = document.querySelector("#alarm");
     alert("Page loaded");
--- script.ts
+++ script.ts
@@ -1,6 +1,3 @@
-console.log("works");
-alert("works");
-
 document.addEventListener("DOMContentLoaded", function() {
     let button = document.querySelector("#alarm");
     alert("Page loaded");
