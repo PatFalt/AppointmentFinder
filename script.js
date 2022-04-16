990bda0e1208b0597eb9124d119028139899829a
M.Porubsky <porubsky1995@hotmail.com>
2022-04-16 13:30:48 +0200

    setup work
    adding a form and minor work

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
+    let form = document.getElementById("eventForm");
+    let submitBttn = document.getElementById("addEvent");
+    form.style.display = "none";
+    form.style.opacity = "0";
+    submitBttn === null || submitBttn === void 0 ? void 0 : submitBttn.addEventListener("click", () => {
+        if (form.style.display == "none") {
+            form.style.display = "block";
+            form.style.opacity = "1";
+            submitBttn.textContent = "Cancel";
+        }
+        else {
+            form.style.display = "none";
+            form.style.opacity = "0";
+            submitBttn.textContent = "Add New Appointment";
+        }
+    });
 });
-
--- script.ts
+++ script.ts
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
+document.addEventListener("DOMContentLoaded", () => {
+    let form =  (<HTMLFormElement>document.getElementById("eventForm"));
+    let submitBttn = (<HTMLButtonElement>document.getElementById("addEvent"));
+    form.style.display = "none";
+    form.style.opacity = "0";
+    submitBttn?.addEventListener("click", () => {
+        if(form.style.display == "none"){
+            form.style.display = "block";
+            form.style.opacity = "1";
+
+            submitBttn.textContent = "Cancel";
+        }
+        else{
+            form.style.display = "none";
+            form.style.opacity = "0";
+        
+            submitBttn.textContent = "Add New Appointment";
+        }
+
+    })
 });
