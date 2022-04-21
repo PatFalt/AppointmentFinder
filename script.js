655b8ebc9062c1c37b07909ac85f565d9e6d51b3
PatFalt <81306186+PatFalt@users.noreply.github.com>
2022-04-22 01:07:29 +0200

    add Event ausgebaut

--- script.js
+++ script.js
@@ -4,6 +4,65 @@ document.addEventListener("DOMContentLoaded", () => {
     let form = document.getElementById("eventForm");
     let addAppt = document.getElementById("addEvent");
     let submit = document.getElementById("submit");
+    let addSlot = document.getElementById("addTimeslot");
+    let timeslots = document.getElementById("timeslots");
+    let optionInput = document.getElementById("optionInput");
+    addAppt.addEventListener("click", () => {
+        $("#newEvent").modal("show");
+    });
+    let count = 0;
+    addSlot.addEventListener("click", () => {
+        $("#optionInput").empty();
+        let today = new Date(Date.now());
+        let dateInputL = document.createElement("label");
+        let timeStartL = document.createElement("label");
+        let timeEndL = document.createElement("label");
+        let dateInput = document.createElement("input");
+        let timeStart = document.createElement("input");
+        let timeEnd = document.createElement("input");
+        let optionbtn = document.createElement("button");
+        dateInput.setAttribute("type", "date");
+        dateInput.setAttribute("id", "date" + count);
+        dateInput.setAttribute("min", today.toString());
+        timeStart.setAttribute("type", "time");
+        timeStart.setAttribute("id", "date" + count);
+        timeEnd.setAttribute("type", "time");
+        timeEnd.setAttribute("id", "date" + count);
+        dateInputL.setAttribute("for", "date" + count);
+        timeStartL.setAttribute("for", "date" + count);
+        timeEndL.setAttribute("for", "date" + count);
+        optionbtn.setAttribute("type", "button");
+        optionbtn.setAttribute("class", "btn btn-secondary btn-sm");
+        dateInputL.innerText = "Date";
+        timeStartL.innerText = "From";
+        timeEndL.innerText = "To";
+        optionbtn.innerText = "Add";
+        console.log(dateInput.getAttribute("id"));
+        //console.log(dateInput.getAttribute("min"));
+        optionInput === null || optionInput === void 0 ? void 0 : optionInput.appendChild(dateInputL);
+        optionInput === null || optionInput === void 0 ? void 0 : optionInput.appendChild(dateInput);
+        optionInput === null || optionInput === void 0 ? void 0 : optionInput.appendChild(timeStartL);
+        optionInput === null || optionInput === void 0 ? void 0 : optionInput.appendChild(timeStart);
+        optionInput === null || optionInput === void 0 ? void 0 : optionInput.appendChild(timeEndL);
+        optionInput === null || optionInput === void 0 ? void 0 : optionInput.appendChild(timeEnd);
+        optionInput === null || optionInput === void 0 ? void 0 : optionInput.appendChild(optionbtn);
+        optionbtn.addEventListener("click", () => {
+            if (dateInput.value != "" && timeStart.value != "" && timeEnd.value != "") {
+                let setDate = new Date(dateInput.value);
+                if (dateInPast(setDate, today)) {
+                    alert("Do you have a time machine?");
+                    return false;
+                }
+                else {
+                    $("#slotList").append("<li>" + dateInput.value + ": from " + timeStart.value + " to " + timeEnd.value + "</li>");
+                }
+            }
+            else {
+                alert("Please fill out all fields");
+            }
+            count++;
+        });
+    });
     submit.addEventListener("click", () => {
         let eventName = $("#eventName").val();
         let endDate = $("#endDate").val();
@@ -22,22 +81,33 @@ document.addEventListener("DOMContentLoaded", () => {
                     description: description
                 },
                 cache: false,
-                success: () => { alert("Event created succesfully"); },
+                success: () => {
+                    alert("Event created succesfully");
+                    $("#eventName").val("");
+                    $("#endDate").val("");
+                    $("#description").val("");
+                },
                 error: () => { console.error("Error"); }
             });
         }
     });
-    form.style.display = "none";
-    addAppt === null || addAppt === void 0 ? void 0 : addAppt.addEventListener("click", () => {
-        if (form.style.display == "none") {
+    /*form.style.display = "none";
+    addAppt?.addEventListener("click", () => {
+        if(form.style.display == "none"){
             form.style.display = "block";
             list.style.display = "none";
             addAppt.textContent = "Cancel";
         }
-        else {
+        else{
             form.style.display = "none";
             list.style.display = "block";
             addAppt.textContent = "Add New Appointment";
         }
-    });
+    })*/
 });
+const dateInPast = function (firstDate, secondDate) {
+    if (firstDate.setHours(0, 0, 0, 0) <= secondDate.setHours(0, 0, 0, 0)) {
+        return true;
+    }
+    return false;
+};
--- script.ts
+++ script.ts
@@ -3,6 +3,63 @@ document.addEventListener("DOMContentLoaded", () => {
     let form =  (<HTMLFormElement>document.getElementById("eventForm"));
     let addAppt = (<HTMLButtonElement>document.getElementById("addEvent"));
     let submit = (<HTMLButtonElement>document.getElementById("submit"));
+    let addSlot = (<HTMLButtonElement>document.getElementById("addTimeslot"));
+    let timeslots = document.getElementById("timeslots");
+    let optionInput = document.getElementById("optionInput");
+    addAppt.addEventListener("click", () => {
+        $("#newEvent").modal("show");      
+    })
+    let count: number = 0;
+    addSlot.addEventListener("click", () => {
+        $("#optionInput").empty();
+        let today = new Date(Date.now());
+        let dateInputL =  document.createElement("label");
+        let timeStartL =  document.createElement("label");
+        let timeEndL =  document.createElement("label");
+        let dateInput = document.createElement("input");
+        let timeStart = document.createElement("input");
+        let timeEnd = document.createElement("input");
+        let optionbtn = document.createElement("button");
+        dateInput.setAttribute("type", "date");
+        dateInput.setAttribute("id", "date"+count);
+        dateInput.setAttribute("min", today.toString());
+        timeStart.setAttribute("type", "time");
+        timeStart.setAttribute("id", "date"+count);
+        timeEnd.setAttribute("type", "time");
+        timeEnd.setAttribute("id", "date"+count);
+        dateInputL.setAttribute("for", "date"+count);
+        timeStartL.setAttribute("for", "date"+count);
+        timeEndL.setAttribute("for", "date"+count);
+        optionbtn.setAttribute("type", "button");
+        optionbtn.setAttribute("class", "btn btn-secondary btn-sm");
+        dateInputL.innerText = "Date";
+        timeStartL.innerText = "From";
+        timeEndL.innerText = "To";
+        optionbtn.innerText = "Add";
+        console.log(dateInput.getAttribute("id"));
+        //console.log(dateInput.getAttribute("min"));
+        optionInput?.appendChild(dateInputL);
+        optionInput?.appendChild(dateInput);
+        optionInput?.appendChild(timeStartL);
+        optionInput?.appendChild(timeStart);
+        optionInput?.appendChild(timeEndL);
+        optionInput?.appendChild(timeEnd);
+        optionInput?.appendChild(optionbtn);
+        optionbtn.addEventListener("click", () => {
+            if(dateInput.value != "" && timeStart.value != "" && timeEnd.value != ""){
+                let setDate = new Date(dateInput.value);
+                if(dateInPast(setDate, today)){
+                    alert("Do you have a time machine?");
+                    return false;
+                } else {
+                    $("#slotList").append("<li>"+ dateInput.value + ": from " + timeStart.value + " to " + timeEnd.value + "</li>")
+                }
+            } else {
+                alert("Please fill out all fields");
+            }
+            count++;
+        }) 
+    })
     submit.addEventListener("click", () => {
         let eventName = $("#eventName").val();
         let endDate = $("#endDate").val();
@@ -20,12 +77,17 @@ document.addEventListener("DOMContentLoaded", () => {
                     description: description
                 },
                 cache: false,
-                success: () => {alert("Event created succesfully");},
+                success: () => {
+                    alert("Event created succesfully");
+                    $("#eventName").val("");
+                    $("#endDate").val("");
+                    $("#description").val("");
+                },
                 error: () => {console.error("Error");}
             })
         }
     })
-    form.style.display = "none";
+    /*form.style.display = "none";
     addAppt?.addEventListener("click", () => {
         if(form.style.display == "none"){
             form.style.display = "block";
@@ -37,6 +99,14 @@ document.addEventListener("DOMContentLoaded", () => {
             list.style.display = "block";
             addAppt.textContent = "Add New Appointment";
         }
-    })
+    })*/
 
 });
+
+const dateInPast = function (firstDate: Date, secondDate: Date) {
+    if (firstDate.setHours(0, 0, 0, 0) <= secondDate.setHours(0, 0, 0, 0)) {
+      return true;
+    }
+  
+    return false;
+  };
